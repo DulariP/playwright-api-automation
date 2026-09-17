35fd496a083b06ee3e8f641e7c80fd24893857f9
dulari <dulari@calcey.com>
2026-09-17 17:59:50 +0530

    Update authData file

--- test-data/authData.ts
+++ test-data/authData.ts
@@ -2,7 +2,7 @@ import { Auth } from "../models/Auth";
 
 export function getAuthData(): Auth {
   return {
-    username: process.env.BOOKER_USERNAME!,
-    password: process.env.BOOKER_PASSWORD!,
+    username: process.env.USERNAME!,
+    password: process.env.PASSWORD!,
   };
 }
