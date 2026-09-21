6db7a7aeffe267f4917e4d14c8d8906ae7879be0
dulari <dulari@calcey.com>
2026-09-21 18:02:43 +0530

    update environment config file

--- config/envConfig.ts
+++ config/envConfig.ts
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
 
-const environment = process.env.ENV || "qa";
+const environment = process.env.TEST_ENV || "qa";
 
 dotenv.config({
-  path: `.env.${environment}`,
+  path: `environments/${environment}.env`,
+  override: false,
 });
 
 export const ENV = {
