72d54d5c57e60bfb09819eebca5c86b67832a086
dulari <dulari@calcey.com>
2026-09-21 19:27:38 +0530

    update config files

--- config/envConfig.ts
+++ config/envConfig.ts
@@ -13,8 +13,8 @@ console.log("BASE_URL AFTER DOTENV:", process.env.BASE_URL);
 
 export const ENV = {
   BASE_URL: process.env.BASE_URL!,
-  USERNAME: process.env.USERNAME!,
-  PASSWORD: process.env.PASSWORD!,
+  BOOKER_USERNAME: process.env.BOOKER_USERNAME!,
+  BOOKER_PASSWORD: process.env.BOOKER_PASSWORD!,
 };
 
 console.log("FINAL ENV:", ENV);
--- test-data/authData.ts
+++ test-data/authData.ts
@@ -2,7 +2,7 @@ import { Auth } from "../models/Auth";
 
 export function getAuthData(): Auth {
   return {
-    username: process.env.USERNAME!,
-    password: process.env.PASSWORD!,
+    username: process.env.BOOKER_USERNAME!,
+    password: process.env.BOOKER_PASSWORD!,
   };
 }
