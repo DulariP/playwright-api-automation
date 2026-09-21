664d33eeb21de0c4b60ada9396fc529e94710289
dulari <dulari@calcey.com>
2026-09-21 18:26:06 +0530

    update config file and yml file

--- config/envConfig.ts
+++ config/envConfig.ts
@@ -2,13 +2,19 @@ import dotenv from "dotenv";
 
 const environment = process.env.TEST_ENV || "qa";
 
-dotenv.config({
+const result = dotenv.config({
   path: `environments/${environment}.env`,
   override: false,
 });
+
+console.log("ENV FILE:", `environments/${environment}.env`);
+console.log("DOTENV RESULT:", result);
+console.log("BASE_URL AFTER DOTENV:", process.env.BASE_URL);
 
 export const ENV = {
   BASE_URL: process.env.BASE_URL!,
   USERNAME: process.env.USERNAME!,
   PASSWORD: process.env.PASSWORD!,
 };
+
+console.log("FINAL ENV:", ENV);
